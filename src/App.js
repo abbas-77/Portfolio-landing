cbe0b20ce250ac7929967cb9ff5a98974c04feb4
Abbas <72013784+abbas-77@users.noreply.github.com>
2024-08-14 16:50:22 +0330

    add content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
     <div className="hero">
       <Header />
+      <div className='container'>
+        <Content />
+      </div>
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Content.css";
+
+function Content() {
+  return (
+    <div>
+      <div className="content">
+        <h1 className="content-inner-bold">
+          <div>Hard work beats talent</div>
+          <br />
+          <p>
+            lorem ipsum dolor init lorem ipsum dolor init lorem ipsum dolor init
+          </p>
+          <button>Explore</button>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
