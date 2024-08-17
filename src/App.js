a4156665ef60aaf28ea3edcdd08d790cc33604e6
Abbas <72013784+abbas-77@users.noreply.github.com>
2024-08-17 08:46:33 +0330

    fix gsap and completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 
+import {gsap, Power3} from 'gsap';
 import React from 'react';
 import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Images from './Images';
-//gsap
-import { gsap, Power3 } from 'gsap';
-
 function App() {
-  //gsap
-  let tl = new gsap.timeline(),
-    ease = Power3.easeOut();
+  let tl = new gsap.timeline();
+  let ease = Power3.easeOut;
   return (
     <div className="hero">
-      <Header timeline={tl} ease={ease} />
-      <div className='container'>
-        <Content timeline={tl} />
-        <Images timeline={tl} ease={ease} />
+      <Header timeline = {tl} ease= {ease}/>
+      <div className="container">
+        <Content timeline = {tl} />
+        <Images timeline = {tl} ease = {ease}/>
       </div>
     </div>
   );
